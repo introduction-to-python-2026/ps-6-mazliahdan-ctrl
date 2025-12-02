6318f5adc3d9f03e524f277a293c043b7efbe118
mazliahdan-ctrl <mazliahdan@gmail.com>
2025-12-02 17:31:15 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,13 +1,17 @@
+def read_file(path):
+    file = open(path)
+    rows = file.readlines()
+    file.close()
+    return rows path = "data/codons.txt"
+rows = read_file(path)
+rows[0]
 def create_codon_dict(file_path):
     codon_to_amino_acid = {}
     rows = read_file(file_path)
 
     for row in rows:
-        row_list = row.strip().split()   
-        if len(row_list) < 3:
-            continue
+        row_list = row.strip().split('\t')
         codon = row_list[0]
-        amino_acid = row_list[2]
+        amino_acid = row_list[2] # Third cell is the amino acid abbreviation
         codon_to_amino_acid[codon] = amino_acid
-
     return codon_to_amino_acid
