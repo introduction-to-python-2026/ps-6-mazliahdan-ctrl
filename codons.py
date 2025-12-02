d42973f6fa13db089126c470000d54fb68d99f76
mazliahdan-ctrl <mazliahdan@gmail.com>
2025-12-02 17:36:07 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -3,12 +3,12 @@ def create_codon_dict(file_path):
     rows = read_file(file_path)
 
     for row in rows:
-        row_list = row.strip().split()  # split on ANY whitespace
-        if len(row_list) < 3:
+        parts = row.strip().split()  
+        if len(parts) < 3:
             continue
 
-        codon = row_list[0]
-        amino_acid = row_list[2]  # this is correct for CODON FULLNAME ABBREV
+        codon = parts[0]
+        amino_acid = parts[2]  
 
         codon_to_amino_acid[codon] = amino_acid
 
