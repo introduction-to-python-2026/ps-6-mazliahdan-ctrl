a2a6baf6bf597472778251e47538d85264365ae7
mazliahdan-ctrl <mazliahdan@gmail.com>
2025-12-02 17:19:16 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -4,8 +4,10 @@ def create_codon_dict(file_path):
 
     for row in rows:
         row_list = row.strip().split('\t')
+        if len(row_list) < 3:
+            continue
         codon = row_list[0]
-        amino_acid = row_list[2] # Third cell is the amino acid abbreviation
+        amino_acid = row_list[2]
         codon_to_amino_acid[codon] = amino_acid
-    return codon_to_amino_acid
 
+    return codon_to_amino_acid
